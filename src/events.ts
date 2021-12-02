d9f206f9ba698f2c60d89c3e9700a571ab84e377
Samuel Melm <samuel.melm@stud.uni-heidelberg.de>
2021-12-02 19:58:28 +0100

    refactor(EventSystem): extract NotificationHandler

--- src/events.ts
+++ src/events.ts
@@ -52,19 +52,71 @@ class RequestHandler {
     }
 }
 
+class NotificationHandler {
+    private handlers: Record<string, Record<string, NotificationHandlerFunc[]>>
+    private genericHandlers: NotificationHandlerFunc[]
+
+    constructor(private events: EventSystem) {
+        this.handlers = {}
+        this.genericHandlers = []
+    }
+
+    public add(channel: string, method: string, handler: NotificationHandlerFunc) {
+        if (!this.handlers[channel]) {
+            this.handlers[channel] = {}
+        }
+
+        if (!this.handlers[channel][method]) {
+            this.handlers[channel][method] = []
+        }
+
+        this.handlers[channel][method].push(handler)
+    }
+
+    public addGeneric(handler: NotificationHandlerFunc) {
+        this.genericHandlers.push(handler)
+    }
+
+    public get(channel: string, method: string): NotificationHandlerFunc[] {
+        const handlers = this.getHandlers(channel, method).concat(this.genericHandlers)
+
+        if (handlers.length === 0 && method !== "undefinded-notification-handler") {
+            this.events.notify({
+                channel: "core",
+                method: "undefinded-notification-handler",
+                message: `could not find any handler for ${channel}/${method}`,
+            })
+        }
+
+        return handlers
+    }
+
+    private getHandlers(channel: string, method: string): NotificationHandlerFunc[] {
+        if (this.hasNotificationHandler(channel, method)) {
+            return this.handlers[channel][method]
+        } else {
+            return []
+        }
+    }
+
+    private hasGenericNotificationHandler() {
+        return this.genericHandlers.length != 0
+    }
+
+    private hasNotificationHandler(channel: string, method: string) {
+        return this.handlers[channel] && this.handlers[channel][method]
+    }
+}
+
 export class EventSystem {
-    private notificationHandlers: Record<string, Record<string, NotificationHandlerFunc[]>>
-
-    private notificationHandlersAll: NotificationHandlerFunc[]
-
     private requestHandler: RequestHandler
+    private notificationHandler: NotificationHandler
     private middlewares: Middleware[]
     private debugging: boolean
 
     constructor(debugging: boolean = false) {
-        this.notificationHandlers = {}
-        this.notificationHandlersAll = []
         this.requestHandler = new RequestHandler(this)
+        this.notificationHandler = new NotificationHandler(this)
         this.middlewares = []
         this.debugging = debugging
     }
@@ -74,22 +126,12 @@ export class EventSystem {
         method: string,
         handler: NotificationHandlerFunc
     ) {
-        if (!this.notificationHandlers[channel]) {
-            this.notificationHandlers[channel] = {}
-        }
-
-        if (!this.notificationHandlers[channel][method]) {
-            this.notificationHandlers[channel][method] = []
-        }
-
-        this.notificationHandlers[channel][method].push(handler)
-
+        this.notificationHandler.add(channel, method, handler)
         this.log("added notification listener for", channel)
     }
 
     public listenForAllNotifications(handler: NotificationHandlerFunc) {
-        this.notificationHandlersAll.push(handler)
-
+        this.notificationHandler.addGeneric(handler)
         this.log("added notification listener for all channels")
     }
 
@@ -121,41 +163,9 @@ export class EventSystem {
 
         let { channel, method } = notification
 
-        if (
-            !this.hasNotificationHandler(channel, method) &&
-            !this.hasGenericNotificationHandler()
-        ) {
-            if (method !== "undefinded-notification-handler") {
-                this.notify({
-                    channel: "core",
-                    method: "undefinded-notification-handler",
-                    message: "notification could not be delivered to anything",
-                    notification,
-                })
-            }
-
-            return
-        }
-
-        for (let subscriber of this.getNotificationHandlers(channel, method)) {
+        for (let subscriber of this.notificationHandler.get(channel, method)) {
             subscriber(notification)
         }
-    }
-
-    private getNotificationHandlers(channel: string, method: string): NotificationHandlerFunc[] {
-        if (this.hasNotificationHandler(channel, method)) {
-            return this.notificationHandlers[channel][method].concat(this.notificationHandlersAll)
-        } else {
-            return this.notificationHandlersAll
-        }
-    }
-
-    private hasGenericNotificationHandler() {
-        return this.notificationHandlersAll.length != 0
-    }
-
-    private hasNotificationHandler(channel: string, method: string) {
-        return this.notificationHandlers[channel] && this.notificationHandlers[channel][method]
     }
 
     private log(...args: any[]) {
